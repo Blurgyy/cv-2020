e2e77c16ac18880f0750417f18695f94a94496a7
Gaoyang Zhang <gy@blurgy.xyz>
2020-12-01 10:57:10 +0800

    Use black background, add title text
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- pa1/main.py
+++ pa1/main.py
@@ -6,11 +6,15 @@ import numpy as np
 import sys
 
 
+def downsample(img: np.ndarray, factor: int = 2) -> np.ndarray:
+    return img[::factor, ::factor,]
+
+
 # :param prog: has possible values 0, 1, 2, 3, indicates drawing's
 #              completeness
 def draw(prog: int, width: int = 1920, height: int = 1080) -> np.ndarray:
-    # Use white background
-    frame = 255 * np.ones((height, width, 3), dtype=np.uint8)
+    # Use black background
+    frame = np.zeros((height, width, 3), dtype=np.uint8)
     print(frame.shape)
 
     if prog <= 1:
@@ -21,25 +25,33 @@ def draw(prog: int, width: int = 1920, height: int = 1080) -> np.ndarray:
 
 # :param prog: has possible values 0, 1.  0 for zju logo, 1 for personal info
 def opening(prog: int, width: int = 1920, height: int = 1080) -> np.ndarray:
-    logofile = "./stuff/zjulogo.jpg"
-    logo = cv.imread(logofile)
+    logofile = "./stuff/zjulogo.png"
+    logo = downsample(cv.imread(logofile), 3)
     logo_width = logo.shape[0]
     logo_height = logo.shape[1]
-    # Use white background
-    frame = 255 * np.ones((height, width, 3), dtype=np.uint8)
+    # Use black background
+    frame = np.zeros((height, width, 3), dtype=np.uint8)
 
     hor_begin = (width - logo_width) // 2
-    ver_begin = (height - logo_height) // 2
+    ver_begin = (height - logo_height) * 1 // 4
     print(frame.shape)
     frame[ver_begin:ver_begin + logo_height,
           hor_begin:hor_begin + logo_width,] = logo
+
+    text = "I have the high ground"
+    fontface = cv.FONT_ITALIC
+    origin = (hor_begin, ver_begin)
+    color = (255, 255, 255)
+    thickness = 2
+
+    cv.putText(frame, text, origin, fontface, 1, color, thickness)
 
     return frame
 
 
 def ending(width: int = 1920, height: int = 1080) -> np.ndarray:
-    # Use white background
-    frame = 255 * np.ones((height, width, 3), dtype=np.uint8)
+    # Use black background
+    frame = np.zeros((height, width, 3), dtype=np.uint8)
     return frame
 
 
