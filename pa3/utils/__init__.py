dd98f787e1990650d173e5371e04a3201f6b5468
Gaoyang Zhang <gy@blurgy.xyz>
2021-01-15 14:10:18 +0800

    Initial commit for pa3
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

---
+++ pa3/utils/__init__.py
@@ -0,0 +1,2 @@
+# Author: Blurgy <gy@blurgy.xyz>
+# Date:   Jan 09 2021, 15:22 [CST]
---
+++ pa3/utils/lenet.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env -S python3 -u
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class LeNet(nn.Module):
+
+    def __init__(self):
+        conv_input = 1
+        conv_hidden1 = 6
+        conv_output = 16
+        fc_input = 6 * 6 * 16
+        fc_h1 = 120
+        fc_h2 = 84
+        fc_output = 10
+
+        super().__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=conv_input,
+                out_channels=conv_hidden1,
+                kernel_size=3,
+                padding=2,
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=conv_hidden1,
+                out_channels=conv_output,
+                kernel_size=3,
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+        )
+        self.fc1 = nn.Sequential(
+            nn.Linear(in_features=fc_input, out_features=fc_h1),
+            nn.ReLU(),
+        )
+        self.fc2 = nn.Sequential(
+            nn.Linear(in_features=fc_h1, out_features=fc_h2),
+            nn.ReLU(),
+        )
+        self.fc3 = nn.Sequential(
+            nn.Linear(in_features=fc_h2, out_features=fc_output),
+            nn.ReLU(),
+        )
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = x.view(x.size()[0], -1)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+        # x = F.max_pool2d(F.relu(self.conv1(x)), 2)
+        # x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        # x = x.view(-1, self.nfeatures(x))
+        # x = F.relu(self.fc1(x))
+        # x = F.relu(self.fc2(x))
+        # x = self.fc3(x)
+        return x
+
+    def nfeatures(self, x):
+        size = x.size()[1:]
+        nfeats = 1
+        for s in size:
+            nfeats *= s
+        return nfeats
+
+
+def main():
+    net = LeNet()
+    x = torch.randn(1, 1, 28, 28)
+    out = net(x)
+    print(out.size())
+    print(out)
+
+
+if __name__ == "__main__":
+    main()
+
+# Author: Blurgy <gy@blurgy.xyz>
+# Date:   Jan 09 2021, 14:52 [CST]
