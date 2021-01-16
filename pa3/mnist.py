010164cdb4e0f3b1890998b6a021f704a79d188d
Gaoyang Zhang <gy@blurgy.xyz>
2021-01-16 12:46:32 +0800

    Add pre-processed augmented MNIST dataset
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- pa3/mnist.py
+++ pa3/mnist.py
@@ -79,7 +79,7 @@ def main():
 
     transform = transforms.ToTensor()
     training_set = datasets.MNIST(
-        './datasets', train=True, transform=transform, download=True)
+        './datasets', train=True, transform=transform, download=False)
     testing_set = datasets.MNIST(
         './datasets', train=False, transform=transform)
     train_loader = torch.utils.data.DataLoader(training_set, **train_kwargs)
