a173b8fddac692ed3a16cc1fd2800e1ebbf8d738
Gaoyang Zhang <gy@blurgy.xyz>
2021-03-09 15:14:16 +0800

    Add documentations for disparity estimation functions
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- final/cpp/include/estimating.hpp
+++ final/cpp/include/estimating.hpp
@@ -7,17 +7,25 @@ void pose_estimation(std::vector<cv::KeyPoint> const &kp1,
                      std::vector<cv::DMatch> const &matches, mat3 const &K,
                      mat3 &R, vec3 &t);
 
-/* Sum of absolute difference (SAD)
+/* Sum of absolute difference (SAD).
  * @param `{l,r}img` **Rectified** stereo images.
  * @param `wr` Window radius, window size is: `wr` * 2 + 1
  * @param `fx` **Effective** focal length on `x` axis of the 2 given rectified
  *        stereo images.
  * @param `conf` Configs.
- * @return Disparity map.
+ * @return Disparity map estimated with minimal SAD.
  */
 cv::Mat SAD(cv::Mat const &left_image, cv::Mat const &right_image,
             int const &wr, MiscConf const &conf);
 
+/* Normalized cross correlation (NCC).
+ * @param `{l,r}img` **Rectified** stereo images.
+ * @param `wr` Window radius, window size is: `wr` * 2 + 1
+ * @param `fx` **Effective** focal length on `x` axis of the 2 given rectified
+ *        stereo images.
+ * @param `conf` Configs.
+ * @return Disparity map estimated with maximum NCC.
+ */
 cv::Mat NCC(cv::Mat const &left_image, cv::Mat const &right_image,
             int const &wr, MiscConf const &conf);
 
