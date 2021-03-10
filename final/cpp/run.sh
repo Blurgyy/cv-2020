64a7c9769dea8813f0cc5888cd47335e0b52133f
Gaoyang Zhang <gy@blurgy.xyz>
2021-03-10 19:54:04 +0800

    Remove run.sh
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- final/cpp/run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env -S bash
-
-if ./build/stereo data/Recycle-perfect/im0.png data/Recycle-perfect/im1.png data/Recycle-perfect/calib.txt; then
-    scp *jpg 10.186.103.185:repos/cv/final/cpp
-fi
-
-# Author: Blurgy <gy@blurgy.xyz>
-# Date:   Mar 07 2021, 22:36 [CST]
